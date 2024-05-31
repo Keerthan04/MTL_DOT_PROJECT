074934caa24d29c8d5bbdea1e03c4525b04cecea
Keerthan Kumar C <ckeerthankumar4@gmail.com>
2024-05-31 10:49:33 +0530

    updated and finalized login page

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ArrowRight } from 'lucide-react';
 import axios from 'axios';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import logo from '../images/tmg-logo.jpg';
+import photo from '../images/mtl_logo.jpg';
 
 function SignInOne() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
   const [error, setError] = useState('');
-  const [token, setToken] = useState(null); // State to store token
+  const [token, setToken] = useState(null);
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
@@ -21,8 +24,8 @@ function SignInOne() {
     .then((res) => {
       if (res.status === 200) {
         const token = res.data.token;
-        setToken(token); // Store the token in state
-        navigate('/home', { state: { token,username }}); // Pass token as state in navigate
+        setToken(token);
+        navigate('/home', { state: { token, username }});
       }
     })
     .catch((err) => {
@@ -37,8 +40,8 @@ function SignInOne() {
   return (
     <section className='w-full h-full'>
       <div className="grid grid-cols-1 lg:grid-cols-2 bg-white w-full h-full">
-        <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
-          <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md border-2 border-gray-300 rounded-lg shadow-lg p-8">
+        <div className="flex w-full h-full items-center justify-center">
+          <div className="mx-auto w-full max-w-sm lg:max-w-md border-2 border-gray-300 rounded-lg shadow-lg p-8 lg:max-w-lg xl:max-w-xl">
             <div className="flex flex-col items-center">
               <img src={logo} alt="school" className="mb-4 w-32 h-32" />
               <h2 className="text-3xl font-bold leading-tight text-center">
@@ -72,16 +75,22 @@ function SignInOne() {
                   <label htmlFor="password" className="text-base font-medium text-gray-900">
                     Password
                   </label>
-                  <div className="mt-2">
+                  <div className="mt-2 relative">
                     <input
                       id="password"
                       className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                      type="password"
+                      type={showPassword ? "text" : "password"}
                       placeholder="Password"
                       value={password}
                       onChange={(e) => setPassword(e.target.value)}
                       required
                     />
+                    <div 
+                      className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? <FaEyeSlash className="text-black" /> : <FaEye className="text-black" />}
+                    </div>
                   </div>
                 </div>
                 <div>
@@ -96,11 +105,11 @@ function SignInOne() {
             </form>
           </div>
         </div>
-        <div className="h-full w-full">
+        <div className="hidden lg:block h-full w-full">
           <img
             className="mx-auto h-full w-full rounded-md object-cover"
-            src="https://images.unsplash.com/photo-1630673245362-f69d2b93880e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-            alt=""
+            src={photo}
+            alt="udayavani main building"
           />
         </div>
       </div>
